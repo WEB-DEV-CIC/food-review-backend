08fed82e54694fbdfae1cdd1c7646c6b071a0aee
PrincessBubblegum007 <chli24@students.oamk.fi>
2025-04-14 18:18:07 +0300

    fix Readme.md

--- index.js
+++ index.js
@@ -24,7 +24,12 @@ const authRoutes = require('./routes/authRoutes');
 const adminRoutes = require('./routes/adminRoutes');
 const reviewRoutes = require('./routes/reviewRoutes');
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173', 
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type']
+}));
 app.use(express.json());
 
 app.use('/api/foods', foodRoutes);
